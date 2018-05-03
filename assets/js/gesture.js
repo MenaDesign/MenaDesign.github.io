134da174d1037e460810c52b911db2ea7b0745c2
Mena_Ye <emnastar329@gmail.com>
2018-05-03 19:37:22 -0400

    Add new portfolio content

--- assets/js/gesture.js
+++ assets/js/gesture.js
@@ -1,66 +1,128 @@
-
 let touchstartX = 0;
 let touchstartY = 0;
 let touchendX = 0;
 let touchendY = 0;
+gestureZone = document.querySelectorAll('.gestureZone');
+WebGestureZone = gestureZone[0];
+designGestureZone = gestureZone[1];
+illustrationGestureZone = gestureZone[2];
 
-gestureZone = document.querySelector('.gestureZone');
+WebGestureZone.addEventListener('touchstart', function(event) {
+        touchstartX = event.changedTouches[0].screenX;
+        touchstartY = event.changedTouches[0].screenY;
+    }
 
-gestureZone.addEventListener('touchstart', function(event) {
-    touchstartX = event.changedTouches[0].screenX;
-    touchstartY = event.changedTouches[0].screenY;
-}, false);
+    , false);
+WebGestureZone.addEventListener('touchend', function(event) {
+        touchendX = event.changedTouches[0].screenX;
+        touchendY = event.changedTouches[0].screenY;
+        handleWebGesture();
+    }
 
-gestureZone.addEventListener('touchend', function(event) {
+    , false);
+designGestureZone.addEventListener('touchstart', function(event) {
+        touchstartX = event.changedTouches[0].screenX;
+        touchstartY = event.changedTouches[0].screenY;
+    }
+
+    , false);
+designGestureZone.addEventListener('touchend', function(event) {
+        touchendX = event.changedTouches[0].screenX;
+        touchendY = event.changedTouches[0].screenY;
+        handleDesignGesture('design');
+    }
+
+    , false);
+illustrationGestureZone.addEventListener('touchstart', function(event) {
+        touchstartX = event.changedTouches[0].screenX;
+        touchstartY = event.changedTouches[0].screenY;
+    }
+
+    , false);
+illustrationGestureZone.addEventListener('touchend', function(event) {
     touchendX = event.changedTouches[0].screenX;
     touchendY = event.changedTouches[0].screenY;
-    handleGesture();
-}, false); 
+    handleDesignGesture('illustration');
+}, false);
 
+function middleGestureLeft(first, second, third) {
+    second.classList.remove('current');
+    second.classList.add('prev');
+    second.classList.add('previous');
+    third.classList.add('current');
+    third.classList.remove('next');
+    first.classList.remove('prev');
+    first.classList.remove('previous');
+    second.classList.remove('prev');
+}
 
-function handleGesture() {
-    if (touchendX >= touchstartX) {
+function middleGestureRight(first, second, third) {
+    first.classList.add("current");
+    second.classList.add("next");
+    first.classList.remove("prev");
+    first.classList.remove("previous");
+    second.classList.remove("current");
+    third.classList.remove("next");
+}
 
-        var prev = document.querySelector('.previous');
-        var current = document.querySelector('.current');
-        var next = document.querySelector('.next');
+function firstGestureLeft(current, next) {
+    current.classList.add('previous');
+    current.classList.remove('current');
+    next.classList.add('current');
+    next.classList.remove('next');
+}
 
+function lastGestureRight(second, third) {
+    second.classList.add("current");
+    third.classList.add("next");
+    third.classList.remove("current");
+    second.classList.remove('previous');
+}
 
-          if(prev){
-            current.classList.add('next');
-            current.classList.remove('current');
-            prev.classList.remove("previous");
-            prev.classList.remove("prev");
-            prev.classList.add("current");
-            if(next){
-               next.classList.remove('next'); 
-            }
-            
-          }
-
-
-    }
-    
+function handleDesignGesture(title) {
+    var first = document.querySelector('.' + title + '-first');
+    var second = document.querySelector('.' + title + '-second');
+    // swipe to left
     if (touchendX <= touchstartX) {
-
-        var prev = document.querySelector('.previous');
-        var current = document.querySelector('.current');
-        var next = document.querySelector('.next');
-
-
-          if(next){
-            current.classList.add('prev');
-            current.classList.add('previous');
-            current.classList.remove('current');
-            next.classList.remove("next");
-            next.classList.add("current");
-            if(prev){
-               prev.classList.remove('prev'); 
-               prev.classList.remove('previous'); 
-            }
-
-          }
-
+        if (first.classList.contains("current")) {
+            firstGestureLeft(first, second);
+        }
     }
-    
+    // swipe to right
+    if (touchendX >= touchstartX) {
+        if (second.classList.contains("current")) {
+            lastGestureRight(first, second);
+        }
+    }
+}
+
+function handleWebGesture() {
+    var prev = document.querySelector('.previous');
+    var current = document.querySelector('.current');
+    var next = document.querySelector('.next');
+    var first = document.querySelector('.first');
+    var second = document.querySelector('.second');
+    var third = document.querySelector('.third');
+    // swipe to left
+    if (touchendX <= touchstartX) {
+        if (current === second) {
+            middleGestureLeft(first, second, third)
+        } else if (current == first) {
+            current.classList.add('prev');
+            firstGestureLeft(current, next);
+            third.classList.add('next');
+        }
+    }
+    // swipe to right
+    if (touchendX >= touchstartX) {
+        if (current === second) {
+            middleGestureRight(first, second, third)
+        }
+        if (current === third) {
+            lastGestureRight(second, third);
+            second.classList.remove("prev");
+            first.classList.add("prev");
+            first.classList.add("previous");
+        }
+    }
 }
